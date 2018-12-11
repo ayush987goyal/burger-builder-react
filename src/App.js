1648e23f704ab761bc1332d4886cdae9a5e08e42
Ayush Goyal <ayush987goyal@gmail.com>
2018-12-12 00:07:57 +0530

    Use React lazy and Suspense

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Suspense, lazy } from 'react';
 import { useStore, useAction } from 'easy-peasy';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
-import asyncComponent from './hoc/asyncComponent/asyncComponent';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Logout from './containers/Auth/Logout/Logout';
+import Spinner from './components/UI/Spinner/Spinner';
 
-const asyncCheckout = asyncComponent(() => {
-  return import('./containers/Checkout/Checkout');
-});
+const asyncCheckout = lazy(() => import('./containers/Checkout/Checkout'));
 
-const asyncOrders = asyncComponent(() => {
-  return import('./containers/Orders/Orders');
-});
+const asyncOrders = lazy(() => import('./containers/Orders/Orders'));
 
-const asyncAuth = asyncComponent(() => {
-  return import('./containers/Auth/Auth');
-});
+const asyncAuth = lazy(() => import('./containers/Auth/Auth'));
 
 const App = () => {
   const isAuthenticated = useStore(state => state.auth.token !== null);
@@ -49,9 +43,9 @@ const App = () => {
   }
 
   return (
-    <div>
-      <Layout>{routes}</Layout>
-    </div>
+    <Layout>
+      <Suspense fallback={<Spinner />}>{routes}</Suspense>
+    </Layout>
   );
 };
 
